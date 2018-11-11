31dd28c2db28058289da5819130e9daf5742a977
Arnaud Ligny <arnaud@ligny.org>
2018-11-11 21:34:50 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -3,7 +3,7 @@ curl -sSOL https://phpoole.org/phpoole.phar
 php phpoole.phar --version
 
 echo "Started PHPoole build"
-if [ -z "$1" ]; then php phpoole.phar build --baseurl=$URL --quiet; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts --quiet; fi
+if [ -z "$1" ]; then php phpoole.phar build --baseurl=$URL --quiet; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 echo "Finished PHPoole build"
 
 exit 0
