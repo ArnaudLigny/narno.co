8cc81965362ed8a0919b9886a928043f4d8f6fb0
Arnaud Ligny <arnaud@ligny.org>
2021-10-07 03:18:16 +0200

    Reset

--- content/l/2016-03-08-mouvance-statique.md
+++
@@ -1,5 +0,0 @@
----
-title: 'La mouvance statique'
-slug: 'mouvance-statique'
-redirect: https://frank.taillandier.me/2016/03/08/les-gestionnaires-de-contenu-statique/
----
--- netlify.sh
+++
@@ -1,9 +0,0 @@
-echo "Downloading Cecil"
-curl -sSOL https://cecil.app/cecil.phar
-php cecil.phar --version
-
-echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --baseurl=$URL --quiet; else echo "URL: $1" && php cecil.phar build --baseurl=$1 --drafts; fi
-echo "Finished Cecil build"
-
-exit 0
